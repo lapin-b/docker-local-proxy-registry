ce6f026a5b96c95a45d23fdde53aa9abab84a258
l4p1n <l4p1n@l4p1n.ch>
2022-12-19 17:52:18 +0100

    Add autogenerated PHPDoc to models with the IDE helper

--- app/Models/PendingContainerLayer.php
+++ app/Models/PendingContainerLayer.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\PendingContainerLayer
+ *
+ * @property string $id
+ * @property string $container_reference
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer whereContainerReference($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PendingContainerLayer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class PendingContainerLayer extends Model
 {
     use HasFactory;
