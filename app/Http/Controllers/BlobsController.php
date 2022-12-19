4883d9f84f42e9235dfaee0ee6a4bd74c4ee08c9
l4p1n <l4p1n@l4p1n.ch>
2022-12-19 10:47:32 +0100

    Partial upload processing, phase 1

--- app/Http/Controllers/BlobsController.php
+++ app/Http/Controllers/BlobsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PendingContainerLayer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Ulid\Ulid;
 
 class BlobsController extends Controller
@@ -24,6 +25,26 @@ class BlobsController extends Controller
                 route('blobs.process_upload', compact('pending_container_layer', 'container_ref'))
             )
             ->header('Range', '0-0')
+            ->header('Docker-Upload-UUID', $pending_container_layer->id);
+    }
+
+    public function processPartialUpload(
+        Request $request,
+        string $container_ref,
+        PendingContainerLayer $pending_container_layer
+    ){
+        if($pending_container_layer->container_reference != $container_ref){
+            return response('', 403);
+        }
+
+        $body = $request->getContent(true);
+        $local_storage = Storage::disk('local');
+        $local_storage->makeDirectory('uploads');
+        $local_storage->writeStream('uploads/' . $pending_container_layer->id, $body);
+        $file_size = $local_storage->size('uploads/' . $pending_container_layer->id);
+
+        return response('', 202)
+            ->header('Range', "0-$file_size")
             ->header('Docker-Upload-UUID', $pending_container_layer->id);
     }
 }
