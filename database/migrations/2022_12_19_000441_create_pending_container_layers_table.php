cdb9c41b3d63f7c7965336f751116be8e6e0d731
l4p1n <l4p1n@l4p1n.ch>
2022-12-19 01:12:25 +0100

    Add pending uploads table

---
+++ database/migrations/2022_12_19_000441_create_pending_container_layers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pending_container_layers', function (Blueprint $table) {
+            $table->string('id');
+            $table->string('container_reference');
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pending_container_layers');
+    }
+};
