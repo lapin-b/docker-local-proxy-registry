eb7bd68eb9b988236e336aef629414e982595a01
l4p1n <l4p1n@l4p1n.ch>
2022-12-19 09:43:34 +0100

    Add error for monolithic uploads

--- app/Http/Controllers/BlobsController.php
+++ app/Http/Controllers/BlobsController.php
@@ -9,6 +9,10 @@ use Ulid\Ulid;
 class BlobsController extends Controller
 {
     public function initiateUpload(Request $request, string $container_ref){
+        if($request->query('digest') != null){
+            return response('Monolithic uploads are not implemented', 501);
+        }
+
         $pending_container_layer = new PendingContainerLayer();
         $pending_container_layer->id = Ulid::generate();
         $pending_container_layer->container_reference = $container_ref;
@@ -19,6 +23,7 @@ class BlobsController extends Controller
                 'Location',
                 route('blobs.process_upload', compact('pending_container_layer', 'container_ref'))
             )
+            ->header('Range', '0-0')
             ->header('Docker-Upload-UUID', $pending_container_layer->id);
     }
 }
