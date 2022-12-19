e2f2ac10842c9fa708052de2ec7c317743e11755
l4p1n <l4p1n@l4p1n.ch>
2022-12-19 17:50:28 +0100

    Rename the blobs upload controller to UploadsController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\BlobsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlobsController;
+use App\Http\Controllers\UploadsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,9 @@ use App\Http\Controllers\BlobsController;
 */
 
 Route::prefix('/v2/')->group(function(){
-    Route::post('/{container_ref}/blobs/uploads', [BlobsController::class, 'initiateUpload'])->name('blobs.init_upload');
-    Route::patch('/{container_ref}/blobs/uploads/{pending_container_layer}', [BlobsController::class, 'processPartialUpload'])
-        ->name('blobs.process_upload');
+    Route::post('/{container_ref}/blobs/uploads', [UploadsController::class, 'initiateUpload'])->name('blobs.init_upload');
+    Route::patch('/{container_ref}/blobs/uploads/{pending_container_layer}', [UploadsController::class, 'process_partial_update'])->name('blobs.process_upload');
+    Route::put('/{container_ref}/blobs/uploads/{pending_container_layer}', [UploadsController::class, 'process_partial_update']);
+
+    Route::get('/{container_ref}/blobs/{blob_ref}', [BlobsController::class, 'get_blob'])->name('blobs.get');
 });
