9fd12e33b0b6f0e113a5dada18d973dd2536c50a
l4p1n <l4p1n@l4p1n.ch>
2022-12-26 21:32:40 +0100

    Create database table to store registry credentials
    One table is to save HTTP Basic credentials against a registry
    while the other keeps Bearer tokens issues by registries' authentication
    services

---
+++ database/migrations/2022_12_25_235124_create_docker_registry_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docker_registry_clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('registry');
+            $table->string('container');
+            $table->dateTimeTz('issued_at');
+            $table->dateTimeTz('expires_at');
+            $table->integer('validity_time');
+            $table->string('access_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docker_registry_clients');
+    }
+};
