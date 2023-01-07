c384758d0f88c19bdd23011f01fe429d811c68ec
l4p1n <l4p1n@l4p1n.ch>
2023-01-07 19:59:42 +0100

    Extract the processing of the layer into its own function.
    Minor refactor for better readability

--- app/Console/Commands/OneShot/ImportManifestLayerRelationships.php
+++ app/Console/Commands/OneShot/ImportManifestLayerRelationships.php
@@ -6,9 +6,13 @@ use App\Models\ContainerLayer;
 use App\Models\ManifestMetadata;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class ImportManifestLayerRelationships extends Command
 {
+    const REGISTRY_OBJECT_BLOBS = 'blobs';
+    const REGISTRY_OBJECT_MANIFESTS = "manifests";
+
     /**
      * The name and signature of the console command.
      *
@@ -49,42 +53,52 @@ class ImportManifestLayerRelationships extends Command
             $manifest_layers = collect($manifest_content['layers']);
             $manifest_layers->push($manifest_content['config']);
 
-            $layers_to_sync_to_manifest = [];
             foreach($manifest_layers as $layer){
-                $layer_hash = $layer['digest'];
-                $layer_size = $layer['size'];
-
-                $this->info("Attaching layer $layer_hash to the manifest");
-
-                $db_layer = ContainerLayer::firstOrNew([
-                    'docker_hash' => $layer_hash,
-                    'container' => $manifest->container,
-                    'registry' => $manifest->registry,
-                ], [
-                    'size' => $layer_size
-                ]);
-
-                if(!$db_layer->exists){
-                    $this->warn("Layer $layer_hash does not exist in the database. Checking storage");
-                    $layer_path = !empty($manifest->registry)
-                        ? "proxy/$manifest->registry/$manifest->container/blobs/$layer_hash"
-                        : "repository/$manifest->container/blobs/$layer_hash";
-
-                    if(!$drive->exists($layer_path)){
-                        $this->error("Layer $layer_hash does not exist in the repository. This indicates a broken repository for this image or a broken database. Skipping");
-                        continue;
-                    }
-
-                    $db_layer->save();
-                }
-
-                $layers_to_sync_to_manifest[] = $db_layer->id;
+                $this->_processImageLayer($layer, $manifest, $drive);
             }
-
-            $manifest->layers()->sync($layers_to_sync_to_manifest);
-            $manifest->save();
         }
 
         return Command::SUCCESS;
+    }
+
+    private function _processImageLayer(
+        array $manifest_layers,
+        ManifestMetadata $manifest,
+        FilesystemAdapter $drive
+    ){
+        $layers_to_sync_to_manifest = [];
+        foreach($manifest_layers as $layer){
+            $layer_hash = $layer['digest'];
+            $layer_size = $layer['size'];
+
+            $this->info("Attaching layer $layer_hash to the manifest");
+
+            $db_layer = ContainerLayer::firstOrNew([
+                'docker_hash' => $layer_hash,
+                'container' => $manifest->container,
+                'registry' => $manifest->registry,
+            ], [
+                'size' => $layer_size
+            ]);
+
+            if(!$db_layer->exists){
+                $this->warn("Layer $layer_hash does not exist in the database. Checking storage");
+                $layer_path = !empty($manifest->registry)
+                    ? "proxy/$manifest->registry/$manifest->container/blobs/$layer_hash"
+                    : "repository/$manifest->container/blobs/$layer_hash";
+
+                if(!$drive->exists($layer_path)){
+                    $this->error("Layer $layer_hash does not exist in the repository. This indicates a broken repository for this image or a broken database. Skipping");
+                    continue;
+                }
+
+                $db_layer->save();
+            }
+
+            $layers_to_sync_to_manifest[] = $db_layer->id;
+        }
+
+        $manifest->layers()->sync($layers_to_sync_to_manifest);
+        $manifest->save();
     }
 }
