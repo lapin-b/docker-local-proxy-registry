7011b0a1dc3321d2f5440df9e3779baf8961321e
l4p1n <l4p1n@l4p1n.ch>
2023-01-07 20:13:28 +0100

    Pass the right data to the processing function

--- app/Console/Commands/OneShot/ImportManifestLayerRelationships.php
+++ app/Console/Commands/OneShot/ImportManifestLayerRelationships.php
@@ -54,61 +54,61 @@ class ImportManifestLayerRelationships extends Command
             $manifest_layers = collect($manifest_content['layers']);
             $manifest_layers->push($manifest_content['config']);
 
+            $layers_to_sync_to_manifest = [];
+
             foreach($manifest_layers as $layer){
-                $this->_processImageLayer($layer, $manifest, $drive);
+                $db_layer = $this->_insertOrCreateImageLayer($layer, $manifest, $drive);
+                $layers_to_sync_to_manifest[] = $db_layer->id ?? -1;
             }
+
+            $manifest->layers()->sync(array_filter($layers_to_sync_to_manifest, fn($i) => $i != -1));
+            $manifest->save();
         }
 
         return Command::SUCCESS;
     }
 
-    private function _processImageLayer(
-        array $manifest_layers,
-        ManifestMetadata $manifest,
+    private function _insertOrCreateImageLayer(
+        array             $layer,
+        ManifestMetadata  $manifest,
         FilesystemAdapter $drive
-    ){
-        $layers_to_sync_to_manifest = [];
-        foreach($manifest_layers as $layer){
-            $layer_hash = $layer['digest'];
-            $layer_size = $layer['size'];
+    ): ?ContainerLayer {
+        $layer_hash = $layer['digest'];
+        $layer_size = $layer['size'];
 
-            $this->info("Attaching layer $layer_hash to the manifest");
+        $this->info("Attaching layer $layer_hash to the manifest");
 
-            $db_layer = ContainerLayer::firstOrNew([
-                'docker_hash' => $layer_hash,
-                'container' => $manifest->container,
-                'registry' => $manifest->registry,
-            ], [
-                'size' => $layer_size
-            ]);
+        $db_layer = ContainerLayer::firstOrNew([
+            'docker_hash' => $layer_hash,
+            'container' => $manifest->container,
+            'registry' => $manifest->registry,
+        ], [
+            'size' => $layer_size
+        ]);
 
-            if(!$db_layer->exists){
-                $this->warn("Layer $layer_hash does not exist in the database. Checking storage");
-                $layer_path = $this->_objectPath(
-                    self::REGISTRY_OBJECT_BLOBS,
-                    $manifest->registry,
-                    $manifest->container,
-                    $layer_hash
-                );
+        if(!$db_layer->exists){
+            $this->warn("Layer $layer_hash does not exist in the database. Checking storage");
+            $layer_path = $this->_objectPath(
+                self::REGISTRY_OBJECT_BLOBS,
+                $manifest->registry,
+                $manifest->container,
+                $layer_hash
+            );
 
-                if(!$drive->exists($layer_path)){
-                    $this->error("Layer $layer_hash does not exist in the repository. This indicates a broken repository for the manifest or a broken database. Skipping");
-                    continue;
-                }
-
-                $db_layer->save();
+            if(!$drive->exists($layer_path)){
+                $this->error("Layer $layer_hash does not exist in the repository. This indicates a broken repository for the manifest or a broken database. Skipping");
+                return null;
             }
 
-            $layers_to_sync_to_manifest[] = $db_layer->id;
+            $db_layer->save();
         }
 
-        $manifest->layers()->sync($layers_to_sync_to_manifest);
-        $manifest->save();
+        return $db_layer;
     }
 
     private function _objectPath(string $objectType, ?string $registry, string $container, string $hash){
         return empty($registry)
             ? "repository/$container/$objectType/$hash"
-            : "proxy/$registry/$objectType/$hash";
+            : "proxy/$registry/$container/$objectType/$hash";
     }
 }
